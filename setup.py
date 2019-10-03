6081513927e415fa99b4942811ef306b5dea157f
Petar Jovanovic <Petar.Jovanovic@MacBook-Air-van-Petar-Jovanovic.local>
2019-10-03 11:51:05 +0200

    version 5 update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PqrUpload5-pkg-petronije",
-    version="0.0.4",
+    version="0.0.5",
     author="Example Author",
     author_email="petronije2002@gmail.com",
     description="A small example package",
