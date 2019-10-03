27c8410bcd22712e209e19b2f9a602fdcf5e7ff5
Petar Jovanovic <Petar.Jovanovic@MacBook-Air-van-Petar-Jovanovic.local>
2019-10-03 11:23:09 +0200

    version 3

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PqrUpload-pkg-petronije",
-    version="0.0.2",
+    name="PqrUpload3-pkg-petronije",
+    version="0.0.3",
     author="Example Author",
     author_email="petronije2002@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/petronije2002/PqrUpload/archive/0.0.2.tar.gz",
+    url="https://github.com/petronije2002/PqrUpload/archive/0.0.3.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
